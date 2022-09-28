2fcba09c5e391b414a153caba10624821a58b38e
Alexandre Moine <alexandre@moine.me>
2022-09-28 16:02:22 +0200

    artifact creation

--- setup-artifact.sh
+++ setup-artifact.sh
@@ -2,7 +2,7 @@
 set -xeu
 
 # This script is intended to be run in a new virtual machine, it has been
-# tested with a fresh minimal installation of Ubuntu 20.04 running on a
+# tested with a fresh minimal installation of Ubuntu 22.04 running on a
 # VirtualBox with 2048 MB of RAM and 15 GB disk (more than the default), and
 # took about 35 minutes.
 
@@ -25,28 +25,12 @@ else
   echo "Opam available"
 fi
 
-echo "*** Running opam init ***"
-export OPAMYES=true
-opam init --shell-setup --dot-profile="~/.bashrc"
-eval $(opam env)
-
-echo "*** Running opam update ***"
-opam update
-
-echo "*** Creating new 4.12.0 switch, or switch to 4.12.0 if it exists ***"
-opam switch 4.12.0 || opam switch create \
-  --repositories=default,coq-released=https://coq.inria.fr/opam/released \
-  4.12.0 \
-  ocaml-base-compiler.4.12.0
-
-echo "*** Downloading and unzipping project.zip ***"
-wget "https://gitlab.inria.fr/fpottier/diamonds/-/archive/master/diamonds-master.tar.gz?path=project" -O project.tar.gz
+echo "*** Downloading and unzipping project.tar.gz ***"
+wget "https://gitlab.inria.fr/amoine/spacelambda/-/archive/main/spacelambda-main.tar.gz" -O project.tar.gz
 tar zxvf project.tar.gz --one-top-level=project --strip-components 1
 cd project/
 
-echo "*** Ask opam to install dependencies (may take several minutes) ***"
-opam install . --deps-only --verbose
-eval $(opam env)
+./setup.sh
 
 echo "*** Compile the project ***"
 echo "If an error appears here, try to open a new terminal and run:"
--- setup.sh
+++ setup.sh
@@ -4,16 +4,18 @@ IFS=$'\n\t'
 
 export OPAMYES=true
 
-# Create a local switch. (We assume opam 2 is installed.)
-
-echo "Creating a local switch..."
-opam switch create . ocaml-base-compiler.4.13.1
-eval "$(opam env --set-switch)"
-
 # Update the package list.
 
 echo "Updating the list of available packages..."
 opam update
+
+# Create a local switch. (We assume opam 2 is installed.)
+
+echo "Creating a local switch..."
+opam switch create \
+  --repositories=default,coq-released=https://coq.inria.fr/opam/released \
+  . ocaml-base-compiler.4.13.1
+eval "$(opam env --set-switch)"
 
 # Install Coq & deps.
 
@@ -21,7 +23,6 @@ echo "Installing Coq 8.15.2..."
 opam install coq.8.15.2
 
 echo "Installing stdpp & Iris"
-opam repo add coq-released https://coq.inria.fr/opam/released
 opam install coq-stdpp.1.7.0 coq-iris.3.6.0 coq-iris-heap-lang.3.6.0
 
 echo "Installing diaframe"
@@ -30,6 +31,3 @@ cd diaframe
 git checkout c84cba84ce7af4da46fe86fb0f3d3dd1e3ed7ba4
 make real-install
 cd ..
-
-echo "Compiling..."
-make -j
