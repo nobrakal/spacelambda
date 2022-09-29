bf6d7391e349c64c2ea0265ab8291b02dbfff1e0
Alexandre Moine <alexandre@moine.me>
2022-09-29 13:18:30 +0200

    typo

--- src/axioms.v
+++ src/axioms.v
@@ -9,5 +9,5 @@ Print Assumptions wp_bin_op.
 Print Assumptions wp_call.
 Print Assumptions wp_mk_clo.
 Print Assumptions wp_call_clo.
-Print Assumptions wp_lad.
+Print Assumptions wp_load.
 Print Assumptions wp_store.
