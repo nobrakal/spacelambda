a4a135d0839f4b09880b332bae687fa1b30018a0
Alexandre Moine <alexandre@moine.me>
2022-09-29 13:18:03 +0200

    add a file to show no axioms

---
+++ src/axioms.v
@@ -0,0 +1,13 @@
+From spacelambda Require Import wp_all wp_closure wp_adequacy.
+
+(* This file shows that the project does not depend on any axiom.
+   It is _not_ built by default. *)
+
+Print Assumptions wp_adequacy.
+Print Assumptions wp_alloc.
+Print Assumptions wp_bin_op.
+Print Assumptions wp_call.
+Print Assumptions wp_mk_clo.
+Print Assumptions wp_call_clo.
+Print Assumptions wp_lad.
+Print Assumptions wp_store.
